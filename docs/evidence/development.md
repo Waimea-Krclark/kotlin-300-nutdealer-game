1ef060d6e14fc85708349123d4d3305d1c9f72b4
Waimea-Krclark <krclark@waimea.school.nz>
2026-03-25 15:11:57 +1300

    Rewrote entire location code, added travelling.

--- docs/evidence/development.md
+++ docs/evidence/development.md
@@ -10,17 +10,17 @@ The development log captures key moments in your application development:
 
 ---
 
-## Date: xx/xx/20xx
+## Date: 20/03/2026
 
-Example description and notes. Example description and notes. Example description and notes. Example description and notes. Example description and notes. Example description and notes.
+The window now displays both the User Interface, and the game graphics, allowing the user to interact with the game and see information needed to play.
 
-![example.png](screenshots/example.png)
+![WindowGUI.png](screenshots/WindowGUI.png)
 
 ---
 
-## Date: xx/xx/20xx
+## Date: 25/03/2026
 
-Example description and notes. Example description and notes. Example description and notes. Example description and notes. Example description and notes. Example description and notes.
+Added locations class and created each location with coordinates for a bounding box, so that when a user clicks, it can find the locations and also use the position of locations for other displays and interactions. 
 
 ![example.png](screenshots/example.png)
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,10 @@ import java.awt.Font
 import java.awt.Image
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
 import javax.swing.*
+import kotlin.math.hypot
+import kotlin.math.sqrt
 
 
 fun ImageIcon.scaled(width: Int, height: Int): ImageIcon = ImageIcon(image.getScaledInstance(width, height, Image.SCALE_SMOOTH))
@@ -31,14 +34,18 @@ class App {
     var name = "Test"
     var score = 0
     var locations = listOf(
-        Location("Nut Den", ImageIcon(ClassLoader.getSystemResource("images/Map.png")).scaled(1080, 524), 82, 233, 420, 505),
-        Location("Old Tree Shack", ImageIcon(ClassLoader.getSystemResource("images/Map.png")).scaled(1080, 524), 111, 287, 75, 247),
-        Location("Tree House", ImageIcon(ClassLoader.getSystemResource("images/Map.png")).scaled(1080, 524), 321, 496, 205, 396),
-        Location("Pine Tower", ImageIcon(ClassLoader.getSystemResource("images/Map.png")).scaled(1080, 524), 578, 693, 74, 272),
-        Location("Hole Home", ImageIcon(ClassLoader.getSystemResource("images/Map.png")).scaled(1080, 524), 746, 921, 335, 463),
-        Location("Cave Manor", ImageIcon(ClassLoader.getSystemResource("images/Map.png")).scaled(1080, 524), 871, 1023, 74, 184)
+        Location("Nut Den", mutableListOf(1,2), ImageIcon(ClassLoader.getSystemResource("images/Map.png")).scaled(1080, 524), 82, 233, 420, 505),
+        Location("Old Tree Shack", mutableListOf(0, 3), ImageIcon(ClassLoader.getSystemResource("images/Map.png")).scaled(1080, 524), 111, 287, 75, 247),
+        Location("Tree House", mutableListOf(0, 3, 4), ImageIcon(ClassLoader.getSystemResource("images/Map.png")).scaled(1080, 524), 321, 496, 205, 396),
+        Location("Pine Tower", mutableListOf(1, 2, 4), ImageIcon(ClassLoader.getSystemResource("images/Map.png")).scaled(1080, 524), 578, 693, 74, 272),
+        Location("Hole Home", mutableListOf(2, 3, 5), ImageIcon(ClassLoader.getSystemResource("images/Map.png")).scaled(1080, 524), 746, 921, 335, 463),
+        Location("Cave Manor", mutableListOf(4), ImageIcon(ClassLoader.getSystemResource("images/Map.png")).scaled(1080, 524), 871, 1023, 74, 184)
     )
 
+    var currentLocation = locations[0]
+    var selectLocation = locations[0]
+    var travelling = false
+    var travelProgress:Double = 0.00
 
     fun scorePoints(points: Int) {
         score += points
@@ -67,8 +74,15 @@ class MainWindow(val app: App) {
     private val backgroundImage = ImageIcon(ClassLoader.getSystemResource("images/Map.png")).scaled(1080, 524)
     private val UIbackgroundLabel = JLabel()
     private val UIBackgroundImage = ImageIcon(ClassLoader.getSystemResource("images/UserInterfaceBackground.png")).scaled(1080, 202)
+    private val locationMarker = JLabel()
+    private val markerImage = ImageIcon(ClassLoader.getSystemResource("images/Marker.png")).scaled(70, 70)
+    private val dealerLocation = JLabel()
+    private val dealerIcon = ImageIcon(ClassLoader.getSystemResource("images/NutDealerIcon.png")).scaled(70, 70)
 
-    private val infoWindow = InfoWindow(this, app)      // Pass app state to dialog too
+    private var locationName = JLabel("Nut Den")
+    private var travelButton = JButton("Travel to")
+    private var travelPopup = JLabel("Travelling...")
+    private val travelTimer = Timer(10, null)
 
     init {
         setupLayout()
@@ -83,17 +97,33 @@ class MainWindow(val app: App) {
 
         gameBackgroundLabel.setBounds(0, 0, 1080, 524)
         UIbackgroundLabel.setBounds(0, 524, 1080, 202)
+        locationName.setBounds(890, 550, 300, 30)
+        travelButton.setBounds(890, 590, 100, 30)
+        travelPopup.setBounds(890, 590, 100, 30)
 
+        locationMarker.setBounds((app.selectLocation.coordXMin+app.selectLocation.coordXMax)/2-35, app.selectLocation.coordYMin-70, 70, 70)
+        dealerLocation.setBounds((app.currentLocation.coordXMin+app.currentLocation.coordXMax)/2-35, app.currentLocation.coordYMax-70, 70, 70)
+
+        panel.add(dealerLocation)
+        panel.add(locationMarker)
+        panel.add(locationName)
+        panel.add(travelButton)
+        panel.add(travelPopup)
         panel.add(gameBackgroundLabel)
         panel.add(UIbackgroundLabel)
-
-
     }
 
     private fun setupStyles() {
         gameBackgroundLabel.icon = backgroundImage
         UIbackgroundLabel.icon = UIBackgroundImage
+        locationMarker.icon = markerImage
+        dealerLocation.icon = dealerIcon
 
+        locationName.font = Font(Font.SANS_SERIF, Font.BOLD, 20)
+        locationName.foreground = Color.BLACK
+
+        travelPopup.font = Font(Font.SANS_SERIF, Font.BOLD, 13)
+        travelPopup.foreground = Color.BLACK
     }
 
     private fun setupWindow() {
@@ -105,35 +135,67 @@ class MainWindow(val app: App) {
     }
 
     private fun setupActions() {
-        gameBackgroundLabel.addMouseListener(object : MouseAdapter() {
+//      Handles selecting location by finding if location of click was in the area of a location
+        gameBackgroundLabel.addMouseListener( handleLocationClick())
+
+        travelButton.addActionListener{ handleTravelClick() }
+        travelTimer.addActionListener{ handleTravelTween() }
+    }
+
+    private fun handleLocationClick(): MouseListener {
+        return object : MouseAdapter() {
             override fun mouseClicked(e: MouseEvent) {
-                // This code runs when the label is clicked
-                println("${e.x}, ${e.y}")
-
                 for (location in app.locations){
-                    if(e.x in location.coordXMin..location.coordXMax && e.y in location.coordYMin..location.coordYMax ){
-                        println(location.name)
+                    if(e.x in location.coordXMin..location.coordXMax && e.y in location.coordYMin..location.coordYMax && !app.travelling){
+                        app.selectLocation = location
+                        updateUI()
                     }
-
                 }
             }
-        })
-
+        }
     }
 
-    private fun handleMainClick() {
-        app.scorePoints(1000)       // Update the app state
-        updateUI()                  // Update this window UI to reflect this
+    private fun handleTravelClick() {
+        app.travelling = true
+        app.travelProgress = 0.00
+        travelTimer.start()
+        updateUI()
     }
 
-    private fun handleInfoClick() {
-        infoWindow.show()
+    private fun handleTravelTween() {
+        val initialX = (app.currentLocation.coordXMin+app.currentLocation.coordXMax)/2-35
+        val initialY = app.currentLocation.coordYMax-70
+        val endX = (app.selectLocation.coordXMin+app.selectLocation.coordXMax)/2-35
+        val endY = app.selectLocation.coordYMax-70
+
+        dealerLocation.setBounds((initialX + (endX - initialX) * app.travelProgress).toInt(), (initialY + (endY - initialY) * app.travelProgress).toInt(), 70, 70 )
+
+
+        //Idk fix
+        app.travelProgress += hypot((initialX + (endX - initialX).toDouble()), (initialY + (endY - initialY).toDouble()))/30000
+
+        if (app.travelProgress >= 1.00){
+            travelTimer.stop()
+            app.travelling = false
+            app.currentLocation = app.selectLocation
+            updateUI()
+        }
     }
 
     fun updateUI() {
+        locationName.text = app.selectLocation.name
+        locationMarker.setBounds((app.selectLocation.coordXMin+app.selectLocation.coordXMax)/2-35, app.selectLocation.coordYMin-70, 70, 70)
+        dealerLocation.setBounds((app.currentLocation.coordXMin+app.currentLocation.coordXMax)/2-35, app.currentLocation.coordYMax-70, 70, 70)
 
+        if (app.locations.indexOf(app.selectLocation) in app.currentLocation.adjacentIndex){ travelButton.isVisible = true}
+        else { travelButton.isVisible = false }
 
-        infoWindow.updateUI()       // Keep child dialog window UI up-to-date too
+        if (app.travelling) {
+            travelPopup.isVisible = true
+            travelButton.isVisible = false
+        }
+        else {travelPopup.isVisible = false}
+
     }
 
     fun show() {
@@ -141,78 +203,6 @@ class MainWindow(val app: App) {
     }
 }
 
-
-/**
- * Info UI window is a child dialog and shows how the
- * app state can be shown / updated from multiple places
- *
- * @param owner the parent frame, used to position and layer the dialog correctly
- * @param app the app state object
- */
-class InfoWindow(val owner: MainWindow, val app: App) {
-    private val dialog = JDialog(owner.frame, "DIALOG TITLE", false)
-    private val panel = JPanel().apply { layout = null }
-
-    private val infoLabel = JLabel()
-    private val resetButton = JButton("Reset")
-
-    init {
-        setupLayout()
-        setupStyles()
-        setupActions()
-        setupWindow()
-        updateUI()
-    }
-
-    private fun setupLayout() {
-        panel.preferredSize = java.awt.Dimension(240, 180)
-
-        infoLabel.setBounds(30, 30, 180, 60)
-        resetButton.setBounds(30, 120, 180, 30)
-
-        panel.add(infoLabel)
-        panel.add(resetButton)
-    }
-
-    private fun setupStyles() {
-        infoLabel.font = Font(Font.SANS_SERIF, Font.PLAIN, 16)
-        resetButton.font = Font(Font.SANS_SERIF, Font.PLAIN, 16)
-    }
-
-    private fun setupWindow() {
-        dialog.isResizable = false                              // Can't resize
-        dialog.defaultCloseOperation = JDialog.HIDE_ON_CLOSE    // Hide upon window close
-        dialog.contentPane = panel                              // Main content panel
-        dialog.pack()
-    }
-
-    private fun setupActions() {
-        resetButton.addActionListener { handleResetClick() }
-    }
-
-    private fun handleResetClick() {
-        app.resetScore()    // Update the app state
-        owner.updateUI()    // Update the UI to reflect this, via the main window
-    }
-
-    fun updateUI() {
-        // Use app properties to display state
-        infoLabel.text = "<html>User: ${app.name}<br>Score: ${app.score} points"
-
-        resetButton.isEnabled = app.score > 0
-    }
-
-    fun show() {
-        val ownerBounds = owner.frame.bounds          // get location of the main window
-        dialog.setLocation(                           // Position next to main window
-            ownerBounds.x + ownerBounds.width + 10,
-            ownerBounds.y
-        )
-
-        dialog.isVisible = true
-    }
-}
-
-class Location(val name: String, val backgroundImage: ImageIcon, val coordXMin: Int, val coordXMax: Int, val coordYMin: Int, val coordYMax: Int) {
+class Location(val name: String, val adjacentIndex: MutableList<Int>, val backgroundImage: ImageIcon, val coordXMin: Int, val coordXMax: Int, val coordYMin: Int, val coordYMax: Int) {
 
 }
